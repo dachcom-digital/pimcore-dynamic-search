ce355e4ff1f0d6210429caa70c994babc52e746a
Stefan Hagspiel <shagspiel@dachcom.ch>
2021-01-12 15:55:48 +0100

    move to global tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,22 +1,8 @@
 <?php
 
-use Pimcore\Bootstrap;
-use DachcomBundle\Test\Util\Autoloader;
+$frameworkPath = getenv('PIMCORE_CODECEPTION_FRAMEWORK');
+$bundleTestPath = getenv('TEST_BUNDLE_TEST_DIR');
 
-include __DIR__ . '/../vendor/autoload.php';
+$bootstrap = sprintf('%s/src/_bootstrap.php', $frameworkPath);
 
-define('PIMCORE_KERNEL_CLASS', '\DachcomBundle\Test\App\TestAppKernel');
-define('PIMCORE_TEST', true);
-
-Bootstrap::setProjectRoot();
-Bootstrap::bootstrap();
-
-/**
- * @var $loader \Composer\Autoload\ClassLoader
- */
-Autoloader::addNamespace('Pimcore\Tests', PIMCORE_PROJECT_ROOT . '/vendor/pimcore/pimcore/tests/_support');
-Autoloader::addNamespace('Pimcore\Model\DataObject', __DIR__ . '/_output/var/classes/DataObject');
-
-if (!defined('TESTS_PATH')) {
-    define('TESTS_PATH', __DIR__);
-}
+include_once $bootstrap;
