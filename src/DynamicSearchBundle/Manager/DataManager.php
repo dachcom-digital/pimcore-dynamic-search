3b9a49e93759fe7eaeff6cc27d8d8735b1a5f711
Stefan Hagspiel <shagspiel@dachcom.ch>
2023-07-07 09:40:44 +0200

    remove provider cache to avoid configuration sharing (#72)

--- src/DynamicSearchBundle/Manager/DataManager.php
+++ src/DynamicSearchBundle/Manager/DataManager.php
@@ -2,7 +2,6 @@
 
 namespace DynamicSearchBundle\Manager;
 
-use DynamicSearchBundle\Configuration\ConfigurationInterface;
 use DynamicSearchBundle\Context\ContextDefinitionInterface;
 use DynamicSearchBundle\Exception\ProviderException;
 use DynamicSearchBundle\Provider\DataProviderInterface;
@@ -10,27 +9,13 @@ use DynamicSearchBundle\Registry\DataProviderRegistryInterface;
 
 class DataManager implements DataManagerInterface
 {
-    protected ConfigurationInterface $configuration;
-    protected DataProviderRegistryInterface $dataProviderRegistry;
-
-    protected array $validProviders;
-
-    public function __construct(
-        ConfigurationInterface $configuration,
-        DataProviderRegistryInterface $dataProviderRegistry
-    ) {
-        $this->configuration = $configuration;
-        $this->dataProviderRegistry = $dataProviderRegistry;
+    public function __construct(protected DataProviderRegistryInterface $dataProviderRegistry)
+    {
     }
 
     public function getDataProvider(ContextDefinitionInterface $contextDefinition, string $providerBehaviour): DataProviderInterface
     {
         $dataProviderName = $contextDefinition->getDataProviderName();
-        $cacheKey = sprintf('%s_%s', $contextDefinition->getName(), $dataProviderName);
-
-        if (isset($this->validProviders[$cacheKey])) {
-            return $this->validProviders[$cacheKey];
-        }
 
         if (is_null($dataProviderName) || !$this->dataProviderRegistry->has($dataProviderName)) {
             throw new ProviderException('Invalid requested data provider', $dataProviderName);
@@ -38,8 +23,6 @@ class DataManager implements DataManagerInterface
 
         $dataProvider = $this->dataProviderRegistry->get($dataProviderName);
         $dataProvider->setOptions($contextDefinition->getDataProviderOptions($providerBehaviour));
-
-        $this->validProviders[$cacheKey] = $dataProvider;
 
         return $dataProvider;
     }
--- src/DynamicSearchBundle/Manager/IndexManager.php
+++ src/DynamicSearchBundle/Manager/IndexManager.php
@@ -2,7 +2,6 @@
 
 namespace DynamicSearchBundle\Manager;
 
-use DynamicSearchBundle\Configuration\ConfigurationInterface;
 use DynamicSearchBundle\Context\ContextDefinitionInterface;
 use DynamicSearchBundle\Exception\ProviderException;
 use DynamicSearchBundle\Filter\FilterInterface;
@@ -13,29 +12,15 @@ use DynamicSearchBundle\Registry\IndexProviderRegistryInterface;
 
 class IndexManager implements IndexManagerInterface
 {
-    protected ConfigurationInterface $configuration;
-    protected IndexProviderRegistryInterface $indexProviderRegistry;
-    protected IndexRegistryInterface $indexRegistry;
-    protected array $validProviders;
-
     public function __construct(
-        ConfigurationInterface $configuration,
-        IndexProviderRegistryInterface $indexProviderRegistry,
-        IndexRegistryInterface $indexRegistry
+        protected IndexProviderRegistryInterface $indexProviderRegistry,
+        protected IndexRegistryInterface $indexRegistry
     ) {
-        $this->configuration = $configuration;
-        $this->indexProviderRegistry = $indexProviderRegistry;
-        $this->indexRegistry = $indexRegistry;
     }
 
     public function getIndexProvider(ContextDefinitionInterface $contextDefinition): IndexProviderInterface
     {
         $indexProviderName = $contextDefinition->getIndexProviderName();
-        $cacheKey = sprintf('%s_%s', $contextDefinition->getName(), $indexProviderName);
-
-        if (isset($this->validProviders[$cacheKey])) {
-            return $this->validProviders[$cacheKey];
-        }
 
         if (is_null($indexProviderName) || !$this->indexProviderRegistry->has($indexProviderName)) {
             throw new ProviderException('Invalid requested index provider', $indexProviderName);
@@ -43,8 +28,6 @@ class IndexManager implements IndexManagerInterface
 
         $indexProvider = $this->indexProviderRegistry->get($indexProviderName);
         $indexProvider->setOptions($contextDefinition->getIndexProviderOptions());
-
-        $this->validProviders[$cacheKey] = $indexProvider;
 
         return $indexProvider;
     }
